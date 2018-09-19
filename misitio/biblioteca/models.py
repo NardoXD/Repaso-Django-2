a72445e4025af8eeb9c7bc683c369f49cf9b53ed
NardoXD <lopezcesar292@gmail.com>
2018-09-19 01:03:14 -0600

    Definicion de modelos
    -Asignacion de ruta para MEDIA
    -Creacion de migraciones
    -Insercion de metodo __str__(self)

--- misitio/biblioteca/models.py
+++ misitio/biblioteca/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+	nombre = models.CharField(max_length=30)
+	domicilio = models.CharField(max_length=50)
+	ciudad = models.CharField(max_length=60)
+	estado = models.CharField(max_length=30)
+	pais = models.CharField(max_length=50)
+	website = models.URLField()
+
+	def __str__(self):
+		return self.nombre
+
+class Autor(models.Model):
+	nombre = models.CharField(max_length=30)
+	apellidos = models.CharField(max_length=40)
+	email = models.EmailField()
+
+	def __str__(self):
+		return "%s %s" %(self.nombre, self.apellidos)
+
+class Libro(models.Model):
+	titulo = models.CharField(max_length=100)
+	autores = models.ManyToManyField(Autor)
+	editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+	fecha_publicacion = models.DateField()
+	portada = models.ImageField(upload_to='portadas')
+
+	def __str__(self):
+		return self.titulo
